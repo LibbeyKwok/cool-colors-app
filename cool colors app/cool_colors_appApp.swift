6e1eef5042811497b2570cb4c220b9fa325dbbcd
Libbey Kwok <libbeykwok8@gmail.com>
2023-07-16 10:49:29 -0700

    Initial Commit

---
+++ cool colors app/cool_colors_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  cool_colors_appApp.swift
+//  cool colors app
+//
+//  Created by Libbey Kwok on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct cool_colors_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
